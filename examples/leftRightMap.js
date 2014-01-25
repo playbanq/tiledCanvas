7cca485191bd07d2d936f47d85fdb10eb34bf3b5
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-25 01:26:19 -0500

    Renamed the example and added tile toggling on clicks.

--- examples/leftRightMap.js
+++
@@ -1,6 +0,0 @@
-var canvas = document.getElementById('tiledCanvas');
-TiledCanvas.extend(canvas, 20);
-canvas.setSize();
-canvas.collisionMatrix.create(0.2);
-canvas.grid.draw();
-canvas.grid.drawSquares();
